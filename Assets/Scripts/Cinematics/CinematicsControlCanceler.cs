d58512063051b27360ceb75d80d526b1bb0b8745
SpencerHerring <herrin348@morrisville.edu>
2019-09-30 13:14:38 -0400

    S6-L63 Cinematic Sence

---
+++ Assets/Scripts/Cinematics/CinematicsControlCanceler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Playables;
+using UnityEngine;
+
+namespace RPG.Cinemtics
+{
+    public class CinematicsControlCanceler : MonoBehaviour
+    {
+
+        private void Start()
+        {
+            GetComponent<PlayableDirector>().played += DisableControl;
+            GetComponent<PlayableDirector>().stopped += EnableControl;
+        }
+
+        void DisableControl(PlayableDirector pd) 
+        {
+            print("Disable Control");
+        }
+
+        void EnableControl(PlayableDirector pd)
+        {
+            print("Enable Control");
+        }
+    }
+}
