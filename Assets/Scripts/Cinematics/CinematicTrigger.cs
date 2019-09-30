dabaa4bae14a62a2ce6051291ab98bcdae73d88a
SpencerHerring <herrin348@morrisville.edu>
2019-09-30 14:01:13 -0400

    S6-L54 FIXED

---
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+
+    public class CinematicTrigger : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            GetComponent<PlayableDirector>().Play();
+        }
+    }
+
+}
